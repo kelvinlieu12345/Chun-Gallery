89da3d134f9a8f7fdbb47d03841e3fb9af36b83f
kelvinlieu12345 <51345212+kelvinlieu12345@users.noreply.github.com>
2020-09-24 10:49:01 +0800

    Update kelvin-vuedotjs.js

--- kelvin-vuedotjs.js
+++ kelvin-vuedotjs.js
@@ -1,6 +1,46 @@
+
 var app = new Vue({
     el: '#app',
     data: {
         msg: 'Hello, I am Kelvin'
-    }
+    },
 });
+
+var something = new Vue({
+    el: '#sth',
+    data:{
+        list: [
+             'apple',
+             'bear' ,
+             'orange',
+             'potato'
+        ],
+        all: [1,2,3,4,5,6,7,8],
+        sth: 'carrot'
+    },
+    methods: {
+        add: function(){
+            this.list.push(this.sth)
+            if(this.list.length > 10){
+                this.list.reverse();
+            }
+            this.shuffle()
+        },
+        remove: function(){
+            this.list.length%2===0 ? this.list.pop() : this.list.shift();
+        },
+
+        shuffle: function(){
+            this.list = _.shuffle(this.list);
+        }
+    }
+})
+
+var see = new Vue({
+    el: '#ani',
+    data: {
+        s: true
+    }
+})
+
+
